a1bbcc25c27414a6501ec4ed0439b84c00a8c05e
PhantomStyle <pro100sto18@gmail.com>
2018-03-13 21:33:47 +0300

    с класса

--- javacore-song-task/src/main/java/ru/liga/App.java
+++ javacore-song-task/src/main/java/ru/liga/App.java
@@ -4,10 +4,14 @@ import ru.liga.songtask.content.Content;
 import ru.liga.songtask.domain.Note;
 import ru.liga.songtask.domain.SimpleMidiFile;
 
-import java.text.SimpleDateFormat;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Всего нот: 15
@@ -34,72 +38,82 @@ import java.util.Map;
  * 11: 2
  */
 public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class );
+
     public static void main(String[] args) {
-        SimpleMidiFile simpleMidiFile = new SimpleMidiFile(Content.ZOMBIE);
-        System.out.println("Всего нот: " + simpleMidiFile.vocalNoteList().size());
-        System.out.println("Длительность (сек): " + simpleMidiFile.durationMs() / 1000);
-        System.out.println("<p>\nАнализ диапазона:");
-        List<Note> notes = simpleMidiFile.vocalNoteList();
-        Note minNote = notes.get(0);
-        Note maxNote = notes.get(0);
-        for(Note note : notes){
-            if(note.sign().higher(maxNote.sign())){
-                maxNote = note;
-            }
-            if(note.sign().lower(minNote.sign())){
-                minNote = note;
-            }
-        }
-        System.out.println("верхняя: " + maxNote.sign().fullName());
-        System.out.println("нижняя: " + minNote.sign().fullName());
-        System.out.println("диапазон: " + maxNote.sign().diffInSemitones(minNote.sign()) + "\n<p>");
+        try (PrintStream out = new PrintStream(new FileOutputStream("filename.txt"))) {
 
-        System.out.println("Анализ длительности нот (мс):");
-        Map<Long, Integer> durations = new HashMap<>();
-        for(Note note : notes){
-            if(durations.containsKey(note.durationTicks())){
-                int temp = durations.get(note.durationTicks());
-                durations.put(note.durationTicks(), ++temp);
+            try{
+                throw new StackOverflowError("stackex");
+            } catch (StackOverflowError e) {
+                e.printStackTrace();
             }
-            else {
-                durations.put(note.durationTicks(), 1);
+            SimpleMidiFile simpleMidiFile = new SimpleMidiFile(Content.ZOMBIE);
+            logger.info("Всего нот: " + simpleMidiFile.vocalNoteList().size());
+            logger.info("Всего нот: " + simpleMidiFile.vocalNoteList().size());
+            logger.info("Длительность (сек): " + simpleMidiFile.durationMs() / 1000);
+            logger.info("<p>\nАнализ диапазона:");
+            List<Note> notes = simpleMidiFile.vocalNoteList();
+            Note minNote = notes.get(0);
+            Note maxNote = notes.get(0);
+            for (Note note : notes) {
+                if (note.sign().higher(maxNote.sign())) {
+                    maxNote = note;
+                }
+                if (note.sign().lower(minNote.sign())) {
+                    minNote = note;
+                }
             }
-        }
-        for (Map.Entry<Long, Integer> entry : durations.entrySet())
-        {
-            System.out.println((int)(entry.getKey() * simpleMidiFile.tickInMs()) + ": " + entry.getValue());
-        }
+            logger.info("верхняя: " + maxNote.sign().fullName());
+            logger.info("нижняя: " + minNote.sign().fullName());
+            logger.info("диапазон: " + maxNote.sign().diffInSemitones(minNote.sign()) + "\n<p>");
 
-        System.out.println("<p>\nАнализ нот по высоте:");
-        Map<String, Integer> heights = new HashMap<>();
-        for(Note note : notes){
-            if(heights.containsKey(note.sign().fullName())){
-                int temp = heights.get(note.sign().fullName());
-                heights.put(note.sign().fullName(), ++temp);
+            logger.info("Анализ длительности нот (мс):");
+            Map<Long, Integer> durations = new HashMap<>();
+            for (Note note : notes) {
+                if (durations.containsKey(note.durationTicks())) {
+                    int temp = durations.get(note.durationTicks());
+                    durations.put(note.durationTicks(), ++temp);
+                } else {
+                    durations.put(note.durationTicks(), 1);
+                }
             }
-            else {
-                heights.put(note.sign().fullName(), 1);
+            for (Map.Entry<Long, Integer> entry : durations.entrySet()) {
+                logger.info((int) (entry.getKey() * simpleMidiFile.tickInMs()) + ": " + entry.getValue());
             }
-        }
-        for (Map.Entry<String, Integer> entry : heights.entrySet())
-        {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
 
-        System.out.println("<p>\nАнализ интервалов:");
-        Map<Integer, Integer> intervals = new HashMap<>();
-        for(int i = 0; i < notes.size() - 1; i++) {
-            int interval = notes.get(i).sign().diffInSemitones(notes.get(i + 1).sign());
-            if (intervals.containsKey(interval)) {
-                int temp = intervals.get(interval);
-                intervals.put(interval, ++temp);
-            } else {
-                intervals.put(interval, 1);
+            logger.info("<p>\nАнализ нот по высоте:");
+            Map<String, Integer> heights = new HashMap<>();
+            for (Note note : notes) {
+                if (heights.containsKey(note.sign().fullName())) {
+                    int temp = heights.get(note.sign().fullName());
+                    heights.put(note.sign().fullName(), ++temp);
+                } else {
+                    heights.put(note.sign().fullName(), 1);
+                }
             }
+            for (Map.Entry<String, Integer> entry : heights.entrySet()) {
+                logger.info(entry.getKey() + ": " + entry.getValue());
+            }
+
+            logger.info("<p>\nАнализ интервалов:");
+            Map<Integer, Integer> intervals = new HashMap<>();
+            for (int i = 0; i < notes.size() - 1; i++) {
+                int interval = notes.get(i).sign().diffInSemitones(notes.get(i + 1).sign());
+                if (intervals.containsKey(interval)) {
+                    int temp = intervals.get(interval);
+                    intervals.put(interval, ++temp);
+                } else {
+                    intervals.put(interval, 1);
+                }
+            }
+            for (Map.Entry<Integer, Integer> entry : intervals.entrySet()) {
+                logger.info(entry.getKey() + ": " + entry.getValue());
+            }
+
         }
-        for (Map.Entry<Integer, Integer> entry : intervals.entrySet())
-        {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ javacore-song-task/src/main/java/ru/liga/StackOverflowError.java
@@ -0,0 +1,6 @@
+package ru.liga;
+
+public class StackOverflowError extends Error {
+    public StackOverflowError(String stackex) {
+    }
+}
