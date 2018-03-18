0380e3a1855d5ba6295fbf4911576bbc4cfd096f
pro10 <pro100sto18@gmail.com>
2018-03-18 17:59:06 +0300

    Тесты v2

--- javacore-song-task/src/test/java/ru/liga/junit/noteTests/NoteTestApp.java
+++ javacore-song-task/src/test/java/ru/liga/junit/noteTests/NoteTestApp.java
@@ -1,6 +1,6 @@
 package ru.liga.junit.noteTests;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import ru.liga.App;
@@ -14,27 +14,28 @@ public class NoteTestApp {
     public void lowestNoteTest() throws IOException {
         String[] args = "D:\\cranberries-zombie.mid analyze -f".split(" ");
         App.main(args);
-        assertThat(Engine.engine.getMaxNote().sign().fullName().equals("E2"));
+        assertEquals(Engine.engine.getMinNote().sign().fullName(), "E2");
     }
 
     @Test
     public void highestNoteTest() throws IOException {
         String[] args = "D:\\cranberries-zombie.mid analyze -f".split(" ");
         App.main(args);
-        assertThat(Engine.engine.getMaxNote().sign().fullName().equals("D3"));
+        System.out.println(Engine.engine.getMaxNote().sign().fullName());
+        assertEquals(Engine.engine.getMaxNote().sign().fullName(), "D3");
     }
 
     @Test
     public void durationTest() throws IOException {
         String[] args = "D:\\cranberries-zombie.mid analyze -f".split(" ");
         App.main(args);
-        assertThat(Engine.simpleMidiFile.durationMs() / 1000 == 310);
+        assertEquals(Engine.simpleMidiFile.durationMs() / 1000, 310);
     }
 
     @Test
     public void amountOfNotesTest() throws IOException {
         String[] args = "D:\\cranberries-zombie.mid analyze -f".split(" ");
         App.main(args);
-        assertThat(Engine.simpleMidiFile.vocalNoteList().size() == 388);
+        assertEquals(Engine.simpleMidiFile.vocalNoteList().size(), 388);
     }
 }
